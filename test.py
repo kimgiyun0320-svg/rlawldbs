2b8d47920f0fbf4bc9082ffc68c859d6987d8563
kimgiyun0320-svg <kimgiyun0320@gmail.com>
2025-11-04 18:17:35 +0900

    첫번째 커밋

---
+++ test.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# cap 파일 불러오기 (예: 탭으로 구분되어 있다면)
+df = pd.read_csv("20250715Align_grid.cap", sep="\t", header=None)
+
+# 예시: 컬럼명 지정
+df.columns = ["Time", "X", "Y", "Intensity1", "Intensity2"]
+
+# CSV로 저장
+df.to_csv("converted_file.csv", index=False)
